214b480b07c8bb80985a67b2ac88afdab55df4d1
vinicius-pascoal <vinicius.pascoal@raio.tech>
2023-05-31 11:39:02 -0300

    agora ultilizando sanfona do bootstrap
    falta arrumar os estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './assets/css/App.css';
-import { Dropdown, NavDropdown } from 'react-bootstrap';
+import { Accordion } from 'react-bootstrap';
+
+import Item from './assets/components/TemplateCestas';
 
 import logo from './assets/imgs/logo.png';
 import cesta from './assets/imgs/cestaExemplo.jpg';
-import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 
 function App() {
   return (
@@ -13,35 +14,11 @@ function App() {
         <h1>Cestas e Afetos</h1>
       </div >
       <div className='produtos'>
-        <Dropdown className='boxCesta'>
-          <Dropdown.Toggle variant="none" id="dropdown-basic" className='bx-in'>
-            <img src={cesta} alt="cesta" className='fotoCesta' />
-            <div>
-              <h3>cesta </h3>
-              <h3>cafe da manha</h3>
-            </div>
-          </Dropdown.Toggle>
-          <Dropdown.Menu className='menu'>
-            <Dropdown.Item className='itens'>
-              <div>
-                <span>contem:</span>
-                <p>flores,cafe,suco,bolo,iogurte,wafle,mini geleia,queijo/presunto,granola,2 frutas,mini paes,mini roscas,croissant</p>
-              </div>
-            </Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item className='nomes'>
-              <h3>escolha o nome na cesta</h3>
-              <div>
-                <button>amor que nao se mede</button>
-                <button>que sorte nossa</button>
-                <button>felicidade</button>
-                <button>gratidao</button>
-                <button>amor</button>
-                <button>celebrar</button>
-              </div>
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+        {/* TODO: Add itens loop (arr.map) */}
+        <Accordion defaultActiveKey="0" className='arcodeon'>
+          <Item key='0' img={cesta} cesta='cafe da manha' price='R$ 100,00' cestaUrl='cafe%20da%20manha' conteudo='flores,cafe,suco,bolo,iogurte,wafle,mini geleia,queijo/presunto,granola,2 frutas,mini paes,mini roscas,croissant' />
+          <Item key='1' img={cesta} cesta='cafe da manha' price='R$ 100,00' cestaUrl='cafe%20da%20manha' conteudo='flores,cafe,suco,bolo,iogurte,wafle,mini geleia,queijo/presunto,granola,2 frutas,mini paes,mini roscas,croissant' />
+        </Accordion>
       </div>
     </div >
   );
--- src/assets/components/TemplateCestas/index.jsx
+++ src/assets/components/TemplateCestas/index.jsx
@@ -1,3 +1,33 @@
-export default function () {
+import { Accordion } from 'react-bootstrap';
 
+export default function Item({ key, img, cesta, price, conteudo, cestaUrl }) {
+  return (
+    <Accordion.Item eventKey={key} className='boxCesta'>
+      <Accordion.Header className='bx-in'>
+        <img src={img} alt="cesta" className='fotoCesta' />
+        <div>
+          <h3>cesta </h3>
+          <h3>{cesta}</h3>
+          <h3>{price}</h3>
+        </div>
+      </Accordion.Header>
+      <Accordion.Body className='menu'>
+        <div className='itens'>
+          <span>contem:</span>
+          <p>{conteudo}</p>
+        </div>
+        <div className='nomes'>
+          <h3>escolha o nome na cesta</h3>
+          <div>
+            <a href={`https://wa.me/+5579988210551?text=ola%20estou%20interessado%20em%20uma%20cesta%20${cestaUrl}%20com%20o%20nome%20amor%20que%20nao%20se%20mede`}>amor que nao se mede</a>
+            <a href={`https://wa.me/+5579988210551?text=ola%20estou%20interessado%20em%20uma%20cesta%20${cestaUrl}%20com%20o%20nome%20que%20sorte%20nossa`}>que sorte nossa</a>
+            <a href={`https://wa.me/+5579988210551?text=ola%20estou%20interessado%20em%20uma%20cesta%20${cestaUrl}%20com%20o%20nome%20felicidade`}>felicidade</a>
+            <a href={`https://wa.me/+5579988210551?text=ola%20estou%20interessado%20em%20uma%20cesta%20${cestaUrl}%20com%20o%20nome%20gratidao`}>gratidao</a>
+            <a href={`https://wa.me/+5579988210551?text=ola%20estou%20interessado%20em%20uma%20cesta%20${cestaUrl}%20com%20o%20nome%20amor`}>amor</a>
+            <a href={`https://wa.me/+5579988210551?text=ola%20estou%20interessado%20em%20uma%20cesta%20${cestaUrl}%20com%20o%20nome%20celebrar`}>celebrar</a>
+          </div>
+        </div>
+      </Accordion.Body>
+    </Accordion.Item>
+  )
 }
