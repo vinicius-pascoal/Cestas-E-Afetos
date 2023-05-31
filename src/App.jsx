11a9a982b67981145a3c8f63aa72323c3aac9691
vinicius-pascoal <vinicius.pascoal@raio.tech>
2023-05-31 02:47:53 -0300

    começo do dropdown
    ta com problema pra sumir e espaçamento

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Dropdown, NavDropdown } from 'react-bootstrap';
 
 import logo from './assets/imgs/logo.png';
 import cesta from './assets/imgs/cestaExemplo.jpg';
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 
 function App() {
   return (
@@ -13,16 +14,31 @@ function App() {
       </div >
       <div className='produtos'>
         <Dropdown className='boxCesta'>
-          <Dropdown.Toggle variant="null" id="dropdown-basic" className='bx-in'>
+          <Dropdown.Toggle variant="none" id="dropdown-basic" className='bx-in'>
             <img src={cesta} alt="cesta" className='fotoCesta' />
             <div>
               <h3>cesta </h3>
               <h3>cafe da manha</h3>
             </div>
           </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item>
-
+          <Dropdown.Menu className='menu'>
+            <Dropdown.Item className='itens'>
+              <div>
+                <span>contem:</span>
+                <p>flores,cafe,suco,bolo,iogurte,wafle,mini geleia,queijo/presunto,granola,2 frutas,mini paes,mini roscas,croissant</p>
+              </div>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className='nomes'>
+              <h3>escolha o nome na cesta</h3>
+              <div>
+                <button>amor que nao se mede</button>
+                <button>que sorte nossa</button>
+                <button>felicidade</button>
+                <button>gratidao</button>
+                <button>amor</button>
+                <button>celebrar</button>
+              </div>
             </Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
