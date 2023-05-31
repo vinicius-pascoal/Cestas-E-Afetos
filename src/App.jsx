818187634b30c2da5e7dcf4ad284c12aa9fb8bb0
vinicius-pascoal <vinicius.pascoal@raio.tech>
2023-05-30 23:20:20 -0300

    inicio area dos produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './assets/css/App.css';
-
+import { Dropdown, NavDropdown } from 'react-bootstrap';
 
 import logo from './assets/imgs/logo.png';
+import cesta from './assets/imgs/cestaExemplo.jpg';
 
 function App() {
   return (
@@ -9,8 +10,23 @@ function App() {
       <div className='head'>
         <img src={logo} alt="logo" />
         <h1>Cestas e Afetos</h1>
-
       </div >
+      <div className='produtos'>
+        <Dropdown className='boxCesta'>
+          <Dropdown.Toggle variant="null" id="dropdown-basic" className='bx-in'>
+            <img src={cesta} alt="cesta" className='fotoCesta' />
+            <div>
+              <h3>cesta </h3>
+              <h3>cafe da manha</h3>
+            </div>
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>
+
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
     </div >
   );
 }
